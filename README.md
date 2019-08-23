f0fae66f08627308a327e036095019f81d69e1bc
Rianasoa <rrianasoaonimalala@gmail.com>
2019-08-23 11:52:18 +0300

    Js2

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # BasicsJavascript
+EXERCICES de JAVASCRIPT 
+Bievenue dans notre project  javaScript
+ ___Comment ça marche___
+Pour démarrer l'application, ouvrez index.html sur votre navigateur :
+Pour tester les différents scripts, vous devez modifier le numéro de script sur la ligne 12 de index.html et actualiser la page sur votre navigateur.
+en bas du tag </body> du fichier index.html
+Les résultats des scripts apparaissent dans la console du navigateur. 
+Notre équipe:
+-RABEARISON Rianasoa Onimalala 
+-RANDRIAMBOATIANA Rado Narindra
--- script_6.js
+++ script_6.js
@@ -1,54 +1,51 @@
-function editArn(arn){
-  let tmpArn = "";
-  for (i = 0; i<arn.length; i++){
-    tmpArn += arn[i];
-    if( i!==0 &&  i!== arn.length-1){
-      if ((i+1)%3 === 0){
-        tmpArn += "-";
-      }
-    }
-  }
-  return tmpArn;
+let proteine = [
+{
+	"Sérine": ["UCU","UCC","UCA","UCG","AGU","AGC"],
+	"Proline": ["CCU","CCC","CCA","CCG"],
+	"Leucine": ["UUA","UUG"],
+	"Phenylaline": ["UUU","UUC"],
+	"Arginine": ["CGU","CGC","CGA","CGG","AGA","AGG"],
+	"Tyrosine": ["UAU","UAC"]
 }
-function acideAmine(codon){
-  if(codon === "UCU" || codon === "UCC" || codon === "UCA" || codon === "UCG" || codon === "AGU" || codon === "AGC"){
-    return "Sérine";
-  }
-  else if(codon === "CCU" || codon === "CCC" || codon === "CCA" || codon === "CCG"){
-    return "Proline";
-  }
-  else if(codon === "UUA" ||codon === "UUG"){
-    return "Leucine";
-  }
-  else if(codon === "UUU" || codon === "UUC"){
-    return "Phénylalanine";
-  }
-  else if(codon === "CGU" || codon === "CGC" || codon === "CGA" || codon === "CGG" || codon === "AGA" || codon === "AGG"){
-    return "Arginine";
-  }
-  else if(codon === "UAU" || codon === "UAC"){
-    return "Tyrosine";
-  }
-  else{
-    return 0;
-  }
-}
+	
+];
 
 let arn = "CCGUCGUUGCGCUACAGC";
+
 let arn1 = "CCUCGCCGGUACUUCUCG";
-let codons = [];
-let arrayOfAcideAmine = [];
 
-arn1 = editArn(arn1);
-
-codons = arn1.split("-");
-
-for(let i=0;i<codons.length;i++){
-  if (acideAmine(codons[i]) != 0){
-    arrayOfAcideAmine.push(acideAmine(codons[i]));
-  }
+let newValue = "";
+for(let i = 0; i<arn.length;i+=3){
+	newValue += arn.substr(i,3) + '-';
 }
-console.log("L'arn a transcrire en protéine : ")
-console.log(arn1);
-console.log("Voici les acides aminés correspondants : ")
-console.log(arrayOfAcideAmine);
+
+let result = "";
+
+proteine.map(p => {
+	newValue.split('-').map(s => {
+		for(let key in p){
+			if(p[key].includes(s)){
+				result += key + '-';
+			}
+		}
+	})
+})
+
+let newValue2 = "";
+for(let i = 0; i<arn1.length;i+=3){
+	newValue2 += arn1.substr(i,3) + '-';
+}
+
+let result1 = "";
+
+proteine.map(p => {
+	newValue2.split('-').map(s => {
+		for(let key in p){
+			if(p[key].includes(s)){
+				result1 += key + '-';
+			}
+		}
+	})
+})
+console.log(`Voici la transcription de l'ARN ${arn} : ${result}`);
+console.log(`Voici la transcription de l'ARN ${arn1} : ${result1}`);
